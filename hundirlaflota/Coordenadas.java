ac052c1f7b68be82f2d6df763b84e16779061674
Ddna1 <151134985+Ddna1@users.noreply.github.com>
2025-12-31 18:50:01 +0100

    Enhance Coordenadas class with getters and setters
    Added getter and setter methods for x and y coordinates.

--- hundirlaflota/Coordenadas.java
+++ hundirlaflota/Coordenadas.java
@@ -5,27 +5,48 @@ import java.util.Objects;
 public class Coordenadas {
 	private int x;
 	private int y;
-	
-	public Coordenadas (int x, int y) {
-		this.x=x;
-		this.y=y;
+
+	public Coordenadas(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
-	
-	//Método imprimir coordenadas
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	// Método imprimir coordenadas
 	public String toString() {
 		return ("fila: " + x + " Columna:" + y);
 	}
-	//Sobreescribimos los metodos equals y hashCode para que funcione el metodo contains, sino solo comparamos posiciones de memoria
+
+	// Sobreescribimos los metodos equals y hashCode para que funcione el metodo
+	// contains, sino solo comparamos posiciones de memoria
 	@Override
 	public boolean equals(Object o) {
-	    if (this == o) return true;
-	    if (o instanceof Coordenadas that) {
-	    return x == that.x && y == that.y;}
-	    return false;
+		if (this == o)
+			return true;
+		if (o instanceof Coordenadas that) {
+			return x == that.x && y == that.y;
+		}
+		return false;
 	}
 
 	@Override
+	// Retorna el identificador (posicion de memoria) de cada objeto,,
 	public int hashCode() {
-	    return Objects.hash(x, y);
+		return Objects.hash(x, y);
 	}
 }
